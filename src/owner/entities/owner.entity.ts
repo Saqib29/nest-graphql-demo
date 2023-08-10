bac57db6ef27f26ba6c5f63f749741282e7e384f
saqib <saqib29111@gmail.com>
2023-08-10 18:57:30 +0600

    Owner database entity and implement owner/pet relationship

--- src/owner/entities/owner.entity.ts
+++ src/owner/entities/owner.entity.ts
@@ -1,7 +1,20 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { type } from 'os';
+import { Pet } from 'src/pets/pet.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class Owner {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field()
+  id: number;
+
+  @Column()
+  @Field()
+  name: string;
+
+  @OneToMany(() => Pet, pet => pet.owner)
+  @Field(type => [Pet], { nullable: true })
+  pets?: Pet[];
 }
